ada0176c1c03f191cc9fcd8201baa15661f51fc2
owencyc <owencyc@gmail.com>
2019-08-13 22:46:11 +0800

    验证码

--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from login import models
+from login import models, forms
 
 
 def index(request):
@@ -11,28 +11,29 @@ def index(request):
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        message = '请检查填写的内容！'
-        if username.strip() and password:
-            # 用户名字符合法性验证
-            # 密码长度验证
-            # 更多的其它验证.....
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+        login_form = forms.UserForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+
             try:
                 user = models.User.objects.get(no=username)
-            except BaseException:
+            except:
                 message = '用户不存在！'
-                return render(request, 'login/login.html', {'message': message})
+                return render(request, 'login/login.html', locals())
 
             if user.password == password:
-                print(username, password)
                 return redirect('/index/')
             else:
                 message = '密码不正确！'
-                return render(request, 'login/login.html', {'message': message})
+                return render(request, 'login/login.html', locals())
         else:
-            return render(request, 'login/login.html', {'message': message})
-    return render(request, 'login/login.html')
+            return render(request, 'login/login.html', locals())
+
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
 
 
 def register(request):
