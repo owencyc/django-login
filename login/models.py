762ec6c51b4ca33068a98128ac1ea6043040aae8
owencyc <owencyc@gmail.com>
2019-08-13 17:06:43 +0800

    login func

--- login/models.py
+++ login/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    gender = (
+        ('male', '男'),
+        ('female', '女')
+    )
+    no = models.CharField(max_length=128, unique=True, primary_key=True)
+    password = models.CharField(max_length=256)
+    mail = models.EmailField(unique=True)
+    sex = models.CharField(max_length=16, choices=gender, default='男')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.no
+
+    class Meta:
+        ordering = ["-create_time"]
+        verbose_name = "用户"
+        verbose_name_plural = "用户"
--- login/views.py
+++ login/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from login import models
+
+
+def index(request):
+    pass
+    return render(request, 'login/index.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        message = '请检查填写的内容！'
+        if username.strip() and password:
+            # 用户名字符合法性验证
+            # 密码长度验证
+            # 更多的其它验证.....
+            try:
+                user = models.User.objects.get(no=username)
+            except BaseException:
+                message = '用户不存在！'
+                return render(request, 'login/login.html', {'message': message})
+
+            if user.password == password:
+                print(username, password)
+                return redirect('/index/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'login/login.html', {'message': message})
+        else:
+            return render(request, 'login/login.html', {'message': message})
+    return render(request, 'login/login.html')
+
+
+def register(request):
+    pass
+    return render(request, 'login/register.html')
+
+
+def logout(request):
+    pass
+    return redirect("/login/")
